1e386274a5c387ca1bbf94e83acda3c9f799569d
adonnart <alexandre.donnart@gmail.com>
2016-06-14 10:20:54 +0200

    Update Annuaire-Affiche-XML.php

--- Annuaire-Affiche-XML.php
+++ Annuaire-Affiche-XML.php
@@ -1,18 +1,18 @@
 <?php
 	$xml = simplexml_load_file('Annuaire/Annuaire-XML.xml') or die("Fichier introuvable.");
-	foreach($xml as $pole){
-		echo "<b>".$pole->intitule."</b><br/>";
+	foreach($xml->children() as $pole1){
+		echo "<b>".$pole1->intitule."</b><br/>";
 		if ($contact->fax != ""){ echo $contact->fax."<br/>"; }
-		foreach($pole as $contact){
+		foreach($pole1 as $contact){
 			if ($contact->poste != ""){ echo $contact->poste."<br/>"; }
 			echo $contact->prenom." ".$contact->nom."<br/>";
 			if ($contact->fixe != ""){ echo $contact->fixe."<br/>"; }
 			if ($contact->portable != ""){ echo $contact->portable."<br/>"; }
 			
-			foreach($pole as $pole){
-				echo "<b>".$pole->intitule."</b><br/>";
+			foreach($pole1->children() as $pole2){
+				echo "<b>".$pole2->intitule."</b><br/>";
 				if ($contact->fax != ""){ echo $contact->fax."<br/>"; }
-				foreach($pole as $contact){
+				foreach($pole2 as $contact){
 					if ($contact->poste != ""){ echo $contact->poste."<br/>"; }
 					echo $contact->prenom." ".$contact->nom."<br/>";
 					if ($contact->fixe != ""){ echo $contact->fixe."<br/>"; }
