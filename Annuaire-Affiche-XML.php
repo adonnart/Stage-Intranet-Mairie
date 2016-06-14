d3c6f1f0cb603def01adf6723beb1429cf75f067
adonnart <alexandre.donnart@gmail.com>
2016-06-14 10:41:29 +0200

    Update Annuaire-Affiche-XML.php

--- Annuaire-Affiche-XML.php
+++ Annuaire-Affiche-XML.php
@@ -1,25 +1,18 @@
 <?php
-	$xml = simplexml_load_file('Annuaire/Annuaire-XML.xml') or die("Fichier introuvable.");
-	foreach($xml->children() as $pole){
-		echo "<b>".$pole->intitule."</b><br/>";
-		if ($contact->fax != ""){ echo $contact->fax."<br/>"; }
-		foreach($pole as $contact){
-			if ($contact->poste != ""){ echo $contact->poste."<br/>"; }
-			echo $contact->prenom." ".$contact->nom."<br/>";
-			if ($contact->fixe != ""){ echo $contact->fixe."<br/>"; }
-			if ($contact->portable != ""){ echo $contact->portable."<br/>"; }
-			
-			foreach($pole->children() as $pole){
-				echo "<b>".$pole->intitule."</b><br/>";
-				if ($contact->fax != ""){ echo $contact->fax."<br/>"; }
-				foreach($pole as $contact){
-					if ($contact->poste != ""){ echo $contact->poste."<br/>"; }
-					echo $contact->prenom." ".$contact->nom."<br/>";
-					if ($contact->fixe != ""){ echo $contact->fixe."<br/>"; }
-					if ($contact->portable != ""){ echo $contact->portable."<br/>"; }
-				}
+	function affichePole(){
+		foreach($xml->children() as $pole){
+			echo "<b>".$pole->intitule."</b><br/>";
+			if ($contact->fax != ""){ echo $contact->fax."<br/>"; }
+			foreach($pole as $contact){
+				if ($contact->poste != ""){ echo $contact->poste."<br/>"; }
+				echo $contact->prenom." ".$contact->nom."<br/>";
+				if ($contact->fixe != ""){ echo $contact->fixe."<br/>"; }
+				if ($contact->portable != ""){ echo $contact->portable."<br/>"; }
+				echo "<br/>";
 			}
+			affichePole();
 		}
-		echo "<br/>";
 	}
+	$xml = simplexml_load_file('Annuaire/Annuaire-XML.xml') or die("Fichier introuvable.");
+	affichePole();
 ?>
