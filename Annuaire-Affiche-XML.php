c48e320c07650e1666bb81811e1e95ea211f39d6
adonnart <alexandre.donnart@gmail.com>
2016-06-14 11:26:51 +0200

    Update Annuaire-Affiche-XML.php

--- Annuaire-Affiche-XML.php
+++ Annuaire-Affiche-XML.php
@@ -10,9 +10,9 @@
 			if ($contact->portable != ""){ echo $contact->portable."<br/>"; }
 			echo "<br/>";
 		}
-		if isset($pole2){
+		if (property_exists($pole1,'pole2')){
 			foreach($pole1 as $pole2){
-				echo "<b>".$pole2->intitule."</b>";
+				echo "<b>".$pole2->intitule."</b><br/>";
 				if ($pole2->fax != ""){ echo $pole2->fax."<br/>"; }
 				foreach($pole2 as $contact){
 					if ($contact->poste != ""){ echo $contact->poste."<br/>"; }
