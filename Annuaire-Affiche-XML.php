f22eb112fc307cfbf5dcec640abe55bc257538a2
adonnart <alexandre.donnart@gmail.com>
2016-06-14 14:34:27 +0200

    Update Annuaire-Affiche-XML.php

--- Annuaire-Affiche-XML.php
+++ Annuaire-Affiche-XML.php
@@ -6,20 +6,21 @@
 	<body>
 	<?php
 		function afficheContacts($pole){
-			echo "<table><tr><b>".$pole->intitule."</b></tr><br/>";
+			echo "<tr><b>".$pole->intitule."</b></tr>";
 			if ($pole->fax != ""){ echo "<br/><br/><tr>".$pole->fax."</tr><br/>"; }
 			foreach($pole as $contact){
 				echo "<tr>";
-				if ($contact->poste != ""){	echo "<i style='color:red;'>".$contact->poste."</i><br/>"; }
-				foreach($contact as $personne){	echo $personne->prenom." ".$personne->nom."<br/>"; }
-				if ($contact->fixe != ""){	echo $contact->fixe."<br/>"; }
-				if ($contact->portable != ""){	echo $contact->portable."<br/>"; }
-				echo "</tr><br/>";
+				if ($contact->poste != "")	echo "<td><i>".$contact->poste."</i></td>"; }
+				foreach($contact as $personne){	echo "<td>" $personne->prenom." ".$personne->nom."</td>"; }
+				if ($contact->fixe != ""){	echo "<td>" $contact->fixe."</td>"; }
+				if ($contact->portable != ""){	echo "<td>" $contact->portable."</td>"; }
+				echo "</tr>";
 			}
-			echo "</table>";
+			//echo "</tr>";
 		}
 	
 		$xml = simplexml_load_file('Annuaire/Annuaire-XML.xml') or die("Fichier introuvable.");
+		echo "<table>";
 		foreach($xml as $pole1){
 			afficheContacts($pole1);
 			if (property_exists($pole1,'pole2')){
@@ -28,6 +29,7 @@
 				}
 			}
 		}
+		echo "</table>";
 	?>
 	</body>
 </html>
