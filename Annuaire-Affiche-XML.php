2c8cf6468d4553682e27719440e2bc0d02603935
adonnart <alexandre.donnart@gmail.com>
2016-06-14 15:33:08 +0200

    Update Annuaire-Affiche-XML.php

--- Annuaire-Affiche-XML.php
+++ Annuaire-Affiche-XML.php
@@ -1,16 +1,18 @@
 <html><head><title>Annuaire</title><link rel="stylesheet" content="text/css" href="Annuaire/Annuaire-XML.css" /></head><body>
 <?php
 	function afficheContacts($pole){
-		echo "<tr><td>".$pole->intitule."</td></tr>";
-		if (property_exists($pole,'fax')){ echo "<tr><td>".$pole->fax."</td></tr>"; }
+		echo "<caption>".$pole->intitule."</caption>";
+		if (!empty($pole->fax)){ echo "<tr><td>".$pole->fax."</td></tr>"; }
 		foreach($pole as $contact){
 			echo "<tr>";
-			if (property_exists($contact,'poste')){	echo "<td><i>".$contact->poste."</i></td>"; }
-			foreach($contact as $personne){ if (property_exists($contact,'personne')){
-				echo "<td>".$personne->prenom." ".$personne->nom."</td>"; }
+			if (!empty($contact->poste)){	echo "<td><i>".$contact->poste."</i></td>"; }
+			foreach($contact as $personne){
+				echo "<td>";
+				if (!empty($contact->personne)){ echo $personne->prenom." ".$personne->nom." "; }
+				echo "</td>";
 			}
-			if (property_exists($contact,'fixe')){ echo "<td>".$contact->fixe."</td>"; }
-			if (property_exists($contact,'portable')){ echo "<td>".$contact->portable."</td>"; }
+			if (!empty($contact->fixe)){ echo "<td>".$contact->fixe."</td>"; }
+			if (!empty($contact->portable)){ echo "<td>".$contact->portable."</td>"; }
 			echo "</tr>";
 		}
 	}
@@ -21,7 +23,7 @@
 		echo "<br/><table>";
 		afficheContacts($pole1);
 		foreach($pole1 as $pole2){
-			if (property_exists($pole1,'pole2')){
+			if (!empty($pole1->pole2)){
 				afficheContacts($pole2);
 			}
 		}
