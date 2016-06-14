eb41937f70cb29cc3f02becb4fdd8cce9d2840af
adonnart <alexandre.donnart@gmail.com>
2016-06-14 14:35:53 +0200

    Update Annuaire-Affiche-XML.php

--- Annuaire-Affiche-XML.php
+++ Annuaire-Affiche-XML.php
@@ -10,7 +10,7 @@
 			if ($pole->fax != ""){ echo "<br/><br/><tr>".$pole->fax."</tr><br/>"; }
 			foreach($pole as $contact){
 				echo "<tr>";
-				if ($contact->poste != "")	echo "<td><i>".$contact->poste."</i></td>"; }
+				if ($contact->poste != ""){	echo "<td><i>".$contact->poste."</i></td>"; }
 				foreach($contact as $personne){	echo "<td>" $personne->prenom." ".$personne->nom."</td>"; }
 				if ($contact->fixe != ""){	echo "<td>" $contact->fixe."</td>"; }
 				if ($contact->portable != ""){	echo "<td>" $contact->portable."</td>"; }
@@ -20,16 +20,17 @@
 		}
 	
 		$xml = simplexml_load_file('Annuaire/Annuaire-XML.xml') or die("Fichier introuvable.");
-		echo "<table>";
+		
 		foreach($xml as $pole1){
+			echo "<table>";
 			afficheContacts($pole1);
 			if (property_exists($pole1,'pole2')){
 				foreach($pole1 as $pole2){
 					afficheContacts($pole2);
 				}
 			}
+			echo "</table>";
 		}
-		echo "</table>";
 	?>
 	</body>
 </html>
