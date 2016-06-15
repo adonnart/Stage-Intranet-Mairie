5ae8ee1c70f7771a72209965f699609b25f3a76f
adonnart <alexandre.donnart@gmail.com>
2016-06-15 17:14:26 +0200

    Update and rename Annuaire-Modifier.php to modifier.php

--- modifier.php
+++ modifier.php
@@ -69,3 +69,43 @@ foreach($xml as $pole1){
 ?>
 </div></body></html>
 {/source}
+
+<html>
+	<head></head>
+	<body>
+		<form action="afficheContacts.php" method="get">
+			<select size="1" id="combo">
+  				<optgroup label="Elus municipaux">
+    					<option value="url_11">Lien 1</option>
+    					<option value="url_12">Lien 2</option>
+    					<option value="url_13">Lien 3</option>
+  				</optgroup>
+  				<optgroup label="Adjoints et conseillers délégués">
+    					<option value="url_21">Lien 1</option>
+    					<option value="url_22">Lien 2</option>
+    					<option value="url_23">Lien 3</option>
+  				</optgroup>
+  				<optgroup label="Pôle Direction Générale">
+    					<option value="url_11">Lien 1</option>
+    					<option value="url_12">Lien 2</option>
+    					<option value="url_13">Lien 3</option>
+  				</optgroup>
+  				<optgroup label="Pôle Finances Administration Générale">
+    					<option value="url_11">Lien 1</option>
+    					<option value="url_12">Lien 2</option>
+    					<option value="url_13">Lien 3</option>
+  				</optgroup>
+  				<optgroup label="Catégorie 1">
+    					<option value="url_11">Lien 1</option>
+    					<option value="url_12">Lien 2</option>
+    					<option value="url_13">Lien 3</option>
+  				</optgroup>
+			</select>
+			<input type="submit" value="Ok"/>
+		</form>
+		<br/><br/>
+		<form action="modifContacts.php" method="get">
+			<input type="submit" value="Modifier"/>
+		</form>
+	</body>
+</html>
