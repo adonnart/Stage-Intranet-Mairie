3a2de4852db7be8504b9341a9f45a5fd7eab03e8
adonnart <alexandre.donnart@gmail.com>
2016-06-15 10:08:28 +0200

    Update Annuaire-Affiche-XML.php

--- Annuaire-Affiche-XML.php
+++ Annuaire-Affiche-XML.php
@@ -2,7 +2,7 @@
 <html><head><title>Annuaire</title><link rel="stylesheet" content="text/css" href="Annuaire/Annuaire-XML.css" /></head><body>
 <?php
 	function afficheContacts($pole){
-		echo "<tr><th colspan=10>".$pole->intitule;
+		echo "<tr><th colspan=3>".$pole->intitule;
 		if (!empty($pole->fax)){ echo " - <i>Fax : ".$pole->fax."</i>"; }
 		echo "</th></tr>";
 		foreach($pole as $contact){
@@ -14,14 +14,28 @@
 				}
 				if (!empty($contact->poste)){ echo " - <i>".$contact->poste."</i>"; }
 				echo "</td>";
+				/*
 				if ((!empty($contact->fixe)) and (empty($contact->portable))){
-					echo "<td class='nb'>".$contact->fixe."</td>";
+					echo "<td class='fixe'>".$contact->fixe."</td>";
 				}else if ((empty($contact->fixe)) and (!empty($contact->portable))){
-					echo "<td class='nb'>".$contact->portable."</td>";
+					echo "<td class='portable'>".$contact->portable."</td>";
 				}else{
-					echo "<td class='nbs'>".$contact->fixe.
-					"</td><td class='nbs'>".$contact->portable."</td>";
+					echo "<td class='portable'>".$contact->portable.
+					"</td><td class='fixe'>".$contact->fixe."</td>";
 				}
+				*//*
+				echo "<td class='num'>";
+				if ((!empty($contact->fixe)) and (empty($contact->portable))){
+					echo $contact->fixe."</td>";
+				}else if ((empty($contact->fixe)) and (!empty($contact->portable))){
+					echo $contact->portable."</td>";
+				}else{
+					echo $contact->portable."</td><td class='num'>".$contact->fixe."</td>";
+				}
+				*/
+				echo "<td class='num'>";
+				if (!empty($contact->portable)){ echo $contact->portable."</td><td class='num'>"; }
+				if (!empty($contact->fixe)){ echo $contact->fixe."</td>"; }
 				echo "</tr>";
 			}
 		}
