f20a7e9fbc5609f7bb7287db40cb902dbb768213
adonnart <alexandre.donnart@gmail.com>
2016-06-15 16:08:11 +0200

    Update Annuaire-Affiche-XML.php

--- Annuaire-Affiche-XML.php
+++ Annuaire-Affiche-XML.php
@@ -1,56 +1,69 @@
 {source}
 <html>
 <head><title>Annuaire</title><link rel="stylesheet" content="text/css" href="Annuaire/Annuaire-XML.css" /></head>
-<body><div class='colonnes'>
+<body>
 <?php
-	$file = 'Annuaire/Annuaire-XML.xml';
-	$xml = simplexml_load_file($file) or die("Fichier ".$file." introuvable.");
-	
-	function afficheContacts($pole){
-		foreach($pole as $contact){
-			if (!empty($contact->personne)){
-				echo "<tr>";
-				
-				if (!empty($contact->portable)){ echo "<td class='pers'>"; }
-				else { echo "<td class='pers' colspan=2>"; }
-				
-				foreach($contact as $personne){
-					if (!empty($personne->nom)){ echo $personne->prenom." ".$personne->nom; }
-				}
-				
-				if (!empty($contact->poste)){ echo " - <i>".$contact->poste."</i>"; }
-				echo "</td>";
-				
-				if (!empty($contact->portable)){ echo "<td class='port'>".$contact->portable."</td>"; }
-				if (!empty($contact->fixe)){
-					if (strlen($contact->fixe)<=3){ echo "<td class='fixe'>".$contact->fixe; }
-					else { echo "<td class='port'>".$contact->fixe; }
-				}
-				
-				echo "</td></tr>";
+function afficheContacts($pole){
+	foreach($pole as $contact){
+		if (!empty($contact->personne)){
+			echo "<tr>";
+			
+			if (!empty($contact->portable)){ echo "<td class='pers'>"; }
+			else { echo "<td class='pers' colspan=2>"; }
+			
+			foreach($contact as $personne){
+				if (!empty($personne->nom)){ echo " - ".$personne->prenom." ".$personne->nom; }
 			}
+			
+			if (!empty($contact->poste)){ echo " - <i>".$contact->poste."</i>"; }
+			echo "</td>";
+			/*
+			foreach($contact as $numero){
+				//if (!empty($contact->portable)){ echo "<td class='port'>".$contact->portable."</td>"; }
+				//if (!empty($contact->numero)){
+					echo "<td class='port'>".$numero->numero;
+					if (strlen($contact->numero)<=3){
+						echo "<td class='fixe'>".$contact->numero;
+					}else{
+						echo "<td class='port'>".$contact->numero;
+					}
+					echo "</td>";
+				//}
+			}
+			*/
+			if (!empty($contact->numero)){ echo "<td class='num'>".$contact->numero."</td>"; }
+			if (!empty($contact->numero)){ echo "<td class='num'>".$contact->numero."</td>"; }
+			
+			echo "</tr>";
 		}
 	}
-	
-	foreach($xml as $pole1){
-		echo "<br/><br/><table>";
-		if (!empty($pole1->intitule)){
-			echo "<tr><th colspan=3 class='pole1'>".$pole1->intitule;
-			if (!empty($pole1->fax)){ echo " - <i>Fax : ".$pole1->fax."</i>"; }
-			echo "</th></tr>";
-		}
-		afficheContacts($pole1);
-		foreach($pole1 as $pole2){
-			if (!empty($pole1->pole2)){
-				if (!empty($pole2->intitule)){
-					echo "<tr><th colspan=3 class='pole2'>".$pole2->intitule;
-					if (!empty($pole2->fax)){ echo " - <i>Fax : ".$pole2->fax."</i>"; }
-					echo "</th></tr>";
-				}
-				afficheContacts($pole2);
+}
+
+$file = 'Annuaire/Annuaire-XML.xml';
+$xml = simplexml_load_file($file) or die("Fichier ".$file." introuvable.");
+?>
+<!--<div class="bouton" align="right"><form action="Annuaire/modifier.php" method="get">
+<input class="modifier" type="submit" value="Modifier" /></form></div>-->
+<div class='colonnes'>
+<?php
+foreach($xml as $pole1){
+	echo "<table>";
+	if (!empty($pole1->intitule)){ echo "<tr><th colspan=3 class='pole1'>".$pole1->intitule."</th></tr>"; }
+	afficheContacts($pole1);
+	foreach($pole1 as $pole2){
+		if (!empty($pole1->pole2)){
+			if (!empty($pole2->intitule)){
+				echo "<tr><th colspan=";
+				if (!empty($pole2->fax)){ echo "2"; }else{ echo "3"; }
+				echo " class='pole2'>".$pole2->intitule."</th>";
+				if (!empty($pole2->fax)){ echo "<td class='pole2'>".$pole2->fax."</td>"; }
+				echo "</tr>";
 			}
+			afficheContacts($pole2);
 		}
-		echo "</table>";
 	}
-?></div></body></html>
+	echo "</table>";
+}
+?>
+</div></body></html>
 {/source}
