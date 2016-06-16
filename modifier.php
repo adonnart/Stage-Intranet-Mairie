8b9c34dc423711633d7e599b7f2c85e7acd74169
adonnart <alexandre.donnart@gmail.com>
2016-06-16 14:11:27 +0200

    Update modifier.php

--- modifier.php
+++ modifier.php
@@ -41,17 +41,19 @@
 <html>
 	<!--<head></head>-->
 	<body>
-	<form action="modifier.php" method="get">
+	<form action="" method="get">
         	<select size="1" id="pole1">
 			<option>Elus municipaux</option>
 			<option>Adjoints et conseillers délégués></option>
 			<option>Pôle Direction Générale</option>
 			<option>Pôle Finances Administration Générale</option>
-			<option></option>
-			<option></option>
-			<option></option>
+			<option>Pôle Aménagement Urbanisme et Patrimoine / Travaux</option>
+			<option>Pôle Communication, Culture, Vie associative, Animations et Sport</option>
+			<option>Pôle Education et Action sociale</option>
+			<option>Salles</option>
+			<option>Numéros utiles</option>
 		</select>
-	<input type="submit" value="Modifier"/>
+		<input type="submit" value="Modifier"/>
 	</form>
 	</body>
 </html>
