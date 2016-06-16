5563f81b612b5ffaa8536cbce8537f2e89c4b8b6
adonnart <alexandre.donnart@gmail.com>
2016-06-16 17:26:02 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,16 @@
 <?php
+	$xml = simplexml_load_file('test.xml');
+	$movies = new SimpleXMLElement($xmlstr);
+	
+	$character = $movies->movie[0]->characters->addChild('character');
+	$character->addChild('name', 'Mr. Parser');
+	$character->addChild('actor', 'John Doe');
 
+	$rating = $movies->movie[0]->addChild('rating', 'PG');
+	$rating->addAttribute('type', 'mpaa');
+	
+	echo $movies->asXML('test.xml');
+/*
 	$xmlstr = simplexml_load_file('test.xml');
 	$movies = new SimpleXMLElement($xmlstr);
 	
@@ -11,8 +22,7 @@
 	$rating->addAttribute('type', 'mpaa');
 	
 	echo $movies->asXML('test.xml');
-
-/*
+*//*
 	$dom = new DOMDocument();
 	
 	$dom -> load('test.xml');
@@ -25,40 +35,42 @@
 	
 	echo $dom -> saveXML();
 	$dom -> save('test.xml') or die('XML Manipulate Error');
+
+*//*
+
+	// Set the content type to be XML, so that the browser will   recognise it as XML.
+	header( "content-type: application/xml; charset=ISO-8859-15" );
+	
+	// "Create" the document.
+	$xml = new DOMDocument( "1.0", "ISO-8859-15" );
+	
+	// Create some elements.
+	$xml_album = $xml->createElement( "Album" );
+	$xml_track = $xml->createElement( "Track", "The ninth symphony" );
+	
+	// Set the attributes.
+	$xml_track->setAttribute( "length", "0:01:15" );
+	$xml_track->setAttribute( "bitrate", "64kb/s" );
+	$xml_track->setAttribute( "channels", "2" );
+	
+	// Create another element, just to show you can add any (realistic to computer) number of sublevels.
+	$xml_note = $xml->createElement( "Note", "The last symphony composed by Ludwig van Beethoven." );
+	
+	// Append the whole bunch.
+	$xml_track->appendChild( $xml_note );
+	$xml_album->appendChild( $xml_track );
+	
+	// Repeat the above with some different values..
+	$xml_track = $xml->createElement( "Track", "Highway Blues" );
+	
+	$xml_track->setAttribute( "length", "0:01:33" );
+	$xml_track->setAttribute( "bitrate", "64kb/s" );
+	$xml_track->setAttribute( "channels", "2" );
+	$xml_album->appendChild( $xml_track );
+	
+	$xml->appendChild( $xml_album );
+	
+	// Parse the XML.
+	print $xml->saveXML();
 */
 ?>
-// Set the content type to be XML, so that the browser will   recognise it as XML.
-header( "content-type: application/xml; charset=ISO-8859-15" );
-
-// "Create" the document.
-$xml = new DOMDocument( "1.0", "ISO-8859-15" );
-
-// Create some elements.
-$xml_album = $xml->createElement( "Album" );
-$xml_track = $xml->createElement( "Track", "The ninth symphony" );
-
-// Set the attributes.
-$xml_track->setAttribute( "length", "0:01:15" );
-$xml_track->setAttribute( "bitrate", "64kb/s" );
-$xml_track->setAttribute( "channels", "2" );
-
-// Create another element, just to show you can add any (realistic to computer) number of sublevels.
-$xml_note = $xml->createElement( "Note", "The last symphony composed by Ludwig van Beethoven." );
-
-// Append the whole bunch.
-$xml_track->appendChild( $xml_note );
-$xml_album->appendChild( $xml_track );
-
-// Repeat the above with some different values..
-$xml_track = $xml->createElement( "Track", "Highway Blues" );
-
-$xml_track->setAttribute( "length", "0:01:33" );
-$xml_track->setAttribute( "bitrate", "64kb/s" );
-$xml_track->setAttribute( "channels", "2" );
-$xml_album->appendChild( $xml_track );
-
-$xml->appendChild( $xml_album );
-
-// Parse the XML.
-print $xml->saveXML();
-*/
