818dbe6e151debbb83b80ecc402f199ad9d44e50
adonnart <alexandre.donnart@gmail.com>
2016-06-17 15:11:53 +0200

    Update test-annuaire.php

--- test-annuaire.php
+++ test-annuaire.php
@@ -1,6 +1,5 @@
 {source}
-<html><!--<head></head>--><body>
-	<!--
+<!--<html><head></head><body>
 	<form action="" method="post">
         	<select size="1" id="pole1">
 			<option>Elus municipaux</option>
@@ -16,75 +15,64 @@
 		<input type="submit" value="Modifier"/>
 	</form>
 	-->
-        <?php
-
+<?php
         // Ouverture du fichier.
         $file = 'annuaire/test-annuaire.xml';
         $dom = new DOMDocument();
         $dom -> load($file);
+	//$intitulePole = 'Jeunesse';
+	
+	// Création des noeuds parents
+        $new_contact = $dom -> createElement('contact');
+	$new_personne = $dom -> createElement('personne');
+	
+        $contact = $dom -> getElementsByTagName($new_contact) -> item(0);
+        $personne = $dom -> getElementsByTagName($new_personne) -> item(0);
 
-        // Noeuds parents.
-        $intitulePole = 'Jeunesse';
-        //$intitulePole2 = 'Jeunesse';
+	$nom = $dom -> createElement('nom','GILBERT');
+	$prenom = $dom -> createElement('prenom','Catherine');
+	
+        $personne -> appendChild($nom);
+        $personne -> appendChild($prenom);
 
-        // Création des éléments.
-        $contact = $dom -> createElement('contact');
-        $personne = $dom -> createElement('personne');
-        
-        $nom = $dom -> createElement('nom','GILBERT');
-        $prenom = $dom -> createElement('prenom','Catherine');
-        $portable = $dom -> createElement('portable','06 80 10 70 48');
-        $fixe = $dom -> createElement('fixe','743');
-        
-        //$nomPays = $dom -> createTextNode($valeur);
-        
-        // Ajout des éléments au fichier.
-        
-        $contacts -> appendChild($nom);
-        $contacts -> appendChild($prenom);
-        $personnes = $dom -> getElementsByTagName($personne)->item(0);
-/*      <personne>
-                <nom>GILBERT</nom>
-                <prenom>Catherine</prenom>
-        </personne>
+	$portable = $dom -> createElement('portable','06 80 10 70 48');
+	$fixe = $dom -> createElement('fixe','743');
+	
+	$contact -> appendChild($personne);
+	$contact -> appendChild($portable);
+	$contact -> appendChild($fixe);
+/*      
+	Résultat :
+	<contact>
+		<personne>
+                	<nom>GILBERT</nom>
+                	<prenom>Catherine</prenom>
+        	</personne>
+		<portable>06 80 10 70 48</portable>
+        	<fixe>743</fixe>
+        <contact>
 */
-        $contacts -> appendChild($portable);
-        $contacts -> appendChild($fixe);
-/*      <portable>06 80 10 70 48</portable>
-        <fixe>743</fixe>
-*/
-        $poles2 = $dom -> getElementsByTagName($intitulePole2)->item(0);
-        $tmp = $poles2 -> parentNode; // Récupère le noeud parent de l'intitulé.
-        $tmp -> appendChild($contacts);
-/*      <pole2>
-                [<intitule>Jeunesse</intitule>]
-                <personne>
-                        <nom></nom>
-                        <prenom></prenom>
-                </personne>
+        $intitule = $dom -> getElementsByTagName('Jeunesse') -> item(0);
+        $pole = $intitule -> parentNode; // Récupère le noeud parent de l'intitulé : pole2.
+        $pole -> appendChild($contact);
+/*      
+	Résultat :
+	<pole2>
+		<contact>
+			<personne>
+	                	<nom>GILBERT</nom>
+	                	<prenom>Catherine</prenom>
+	        	</personne>
+			<portable>06 80 10 70 48</portable>
+	        	<fixe>743</fixe>
+	        <contact>
         </pole2>
 */
-        /*$poles1 = $dom -> getElementsByTagName($intitulePole1)->item(0);
-        $tmp1 = $poles2 -> parentNode;
-        $tmp1 -> appendChild($poles2);*/
-
-/*      <pole1>
-                [<intitule>Pôle Education et Action sociale</intitule>]
-                <pole2>
-                        [<intitule>Jeunesse</intitule>]
-                        <personne>
-                                <nom></nom>
-                                <prenom></prenom>
-                        </personne>
-                </pole2>
-        </pole1>
-*/
-        $poles1 = $dom -> getElementsByTagName($pole1)->item(0);
-        $poles1 -> appendChild($poles2);
-        
         // Affichage de tous les éléments 'pays'.
         $liste = $dom -> getElementsByTagName('pole2');
-        foreach ($liste as $tmp) { echo $tmp -> firstChild -> firstChild -> nodeValue . "<br/>"; }
+        foreach ($liste as $tmp) { echo $tmp -> firstChild -> nodeValue . "<br/>"; }
+        
+        //-> firstChild 
         
         //$root = $dom -> documentElement;
         //$element = $dom -> createElement('test','This is the root');
@@ -92,7 +80,6 @@
         
         // Sauvegarde du fichier.
         $dom -> save($file);
+?>
         
-        ?>
-        
-</body></html>{/source}
+<!--</body></html>-->{/source}
