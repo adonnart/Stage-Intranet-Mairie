6d9b7f50c1e355b3be6c6cfe69d6c4df29214539
adonnart <alexandre.donnart@gmail.com>
2016-06-17 14:20:38 +0200

    Update testPays.php

--- testPays.php
+++ testPays.php
@@ -1,33 +1,21 @@
-{source}<!--<html><body>-->
+{source}
 <?php
         // Ouverture du fichier.
-        $file = 'annuaire/test.xml';
+        $file = 'annuaire/testPays.xml';
         $dom = new DOMDocument();
         $dom -> load($file);
 
-        // Définition de l'élément à modifier, de sa valeur et du noeud parent.
-        $element = 'pays';
-        $valeur = 'Cambodge';
-        $noeud = 'asie';
-
-        // Création d'un nouvel élément 'pays'.
-        $nouveauPays = $dom -> createElement($element);
-        $nomPays = $dom -> createTextNode($valeur);
+        $pays = $dom -> createElement('pays','Ukraine');
         
         // Ajout de ce nouvel élément au fichier.
-        $nouveauPays -> appendChild($nomPays);
-        $continent = $dom -> getElementsByTagName($noeud)->item(0);
-        $continent -> appendChild($nouveauPays);
+        $continent = $dom -> getElementsByTagName('europe') -> item(0);
+        $continent -> appendChild($pays);
         
         // Affichage de tous les éléments 'pays'.
         $listePays = $dom -> getElementsByTagName('pays');
         foreach ($listePays as $pays) { echo $pays -> firstChild -> nodeValue . "<br/>"; }
         
-        //$root = $dom -> documentElement;
-        //$element = $dom -> createElement('test','This is the root');
-        //$dom -> appendChild($element);
-        
         // Sauvegarde du fichier.
         $dom -> save($file);
 ?>
-<!--</body></html>-->{/source}
+{/source}
