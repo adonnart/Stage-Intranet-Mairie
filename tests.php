313072688ecb6aa4a4f78391e434ad56d8084d54
adonnart <alexandre.donnart@gmail.com>
2016-06-17 10:12:58 +0200

    Update tests.php

--- tests.php
+++ tests.php
@@ -1,26 +1,25 @@
 {source}<html><body><?php
 
-$dom = new DOMDocument();
-$doc -> load('annuaire/test.xml') or die ("error?");
-$root = $dom -> documentElement;
-
-$listePays = $dom -> getElementsByTagName('pays');
-
-foreach($listePays as $pays){
-echo $pays -> firstChild -> nodeValue . "<br/>";
-}
-
-echo "---<br/>";
-
-$europe = $dom -> getElementsByTagName('europe') -> item(0);
-$listePaysEurope = $europe -> getElementsByTagName('pays');
-foreach($listePaysEurope as $pays){
-echo $pays -> firstChild -> nodeValue . "<br/>";
-}
-
-//$element = $dom -> createElement('test','This is the root');
-//$dom -> appendChild($element);
-
-//$dom -> save('test.xml');
+        $dom = new DOMDocument();
+        $doc -> load('annuaire/test.xml') or die ("error?");
+        $root = $dom -> documentElement;
+        
+        $listePays = $dom -> getElementsByTagName('pays');
+        
+        foreach($listePays as $pays){
+                echo $pays -> firstChild -> nodeValue . "<br/>";
+        }
+        
+        echo "---<br/>";
+        
+        $europe = $dom -> getElementsByTagName('europe') -> item(0);
+        $listePaysEurope = $europe -> getElementsByTagName('pays');
+        foreach($listePaysEurope as $pays){
+                echo $pays -> firstChild -> nodeValue . "<br/>";
+        }
+        
+        //$element = $dom -> createElement('test','This is the root');
+        //$dom -> appendChild($element);
+        //$dom -> save('test.xml');
 
 ?></body></html>{/source}
