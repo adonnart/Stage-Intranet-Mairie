0c26972c1375738a14d8a5e6af7a57a18e95f9b8
adonnart <alexandre.donnart@gmail.com>
2016-06-20 16:44:36 +0200

    Update test-annuaire.php

--- test-annuaire.php
+++ test-annuaire.php
@@ -8,50 +8,40 @@
 	$dom -> validate();
 	echo "Fichier ouvert.<br/>";
 	
-	$poste	= "none";
-	$nom	= "Crèche";
-	$port	= "02 98 00 00 00";
-	$fixe	= "736";
+	$poste	= "";
+	$nom	= "truc";
+	$fixe	= "02 98 11 11 11";
+	$port	= "";
 	
 	$idPole = "Jeunesse";
 	
 	$contact = $dom -> createElement('contact');
 	
-	/*if ($poste != "") { */$contact -> appendChild($dom -> createElement('poste',$poste));// }
-	/*if ($nom != "") { */$contact -> appendChild($dom -> createElement('nom',$nom));// }
-	/*if ($port != "") { */$contact -> appendChild($dom -> createElement('port',$port));// }
-	/*if ($fixe != "") { */$contact -> appendChild($dom -> createElement('fixe',$fixe));// }
+	if ($poste != ""){ $contact -> appendChild($dom -> createElement('poste',$poste)); }
+	if ($nom != "")	 { $contact -> appendChild($dom -> createElement('nom',$nom)); }
+	if ($port != "") { $contact -> appendChild($dom -> createElement('port',$port)); }
+	if ($fixe != "") { $contact -> appendChild($dom -> createElement('fixe',$fixe)); }
 
 	$ids = $dom -> getElementsByTagName('id');
 	
-	echo "<br/>Pôle recherché : ".$idPole."<br/>";
+	echo "<br/>Pôle recherché : ".$idPole."<br/><br/>";
 	
 	$cpt = 0;
-/*
-	foreach($ids as $id){
-		$tmp = $ids -> item($cpt) -> nodeValue;
-		if ($tmp == $idPole) {
-			$pole = $ids -> item($cpt) -> parentNode;
-			//var_dump($pole);
-			echo "<br/>Contact créé.<br/>";
-		}
-		echo $cpt."<br/>".$tmp;
-		$cpt++;
-	}
-*/
 	$check = 0;
 	
 	while ($check == 0) {
 		$tmp = $ids -> item($cpt) -> nodeValue;
+		if ($cpt < 10) { echo "0"; }
 		echo $cpt." - ".$tmp."<br/>";
 		if ($tmp == $idPole) {
 			$pole = $ids -> item($cpt) -> parentNode;
-			//var_dump($pole);
 			$check = 1;
 			echo "<br/>Contact créé.<br/>";
 		}
 		$cpt++;
 	}
+	
+	//$noms = $dom -> getElementsByTagName('nom');
 	
 	$pole -> appendChild($contact);
 
