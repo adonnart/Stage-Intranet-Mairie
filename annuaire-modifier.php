7eb1895249a9a14eae9ddcb935f4c6592a9465aa
adonnart <alexandre.donnart@gmail.com>
2016-06-22 16:04:53 +0200

    Create annuaire-modifier.php

---
+++ annuaire-modifier.php
@@ -0,0 +1,63 @@
+{source}<?php
+
+	$file = 'annuaire/test-v3.xml';
+	
+	$dom = new DOMDocument('1.0');
+	$dom -> preserveWhiteSpace = false;
+	$dom -> load($file);
+	$dom -> validate();
+	echo "Fichier ouvert.<br/>";
+	
+	$poste	= "";
+	$nom	= "truc";
+	$port	= "";
+	$fixe	= "02 98 11 11 11";
+	
+	$idPole = "Jeunesse";
+	
+	$contact = $dom -> createElement('contact');
+	
+	if ($poste != ""){ $contact -> appendChild($dom -> createElement('poste',$poste)); }
+	if ($nom != "")	 { $contact -> appendChild($dom -> createElement('nom',$nom)); }
+	if ($port != "") { $contact -> appendChild($dom -> createElement('port',$port)); }
+	if ($fixe != "") { $contact -> appendChild($dom -> createElement('fixe',$fixe)); }
+
+	$ids = $dom -> getElementsByTagName('id');
+	
+	echo "<br/>Pôle recherché : ".$idPole."<br/><br/>";
+	
+	$cpt = 0;
+	$check = 0;
+	
+	while ($check == 0) {
+		$tmp = $ids -> item($cpt) -> nodeValue;
+		if ($cpt < 10) { echo "0"; }
+		echo $cpt." - ".$tmp."<br/>";
+		if ($tmp == $idPole) {
+			$pole = $ids -> item($cpt) -> parentNode;
+			$check = 1;
+			echo "<br/>Contact créé.<br/>";
+		}
+		$cpt++;
+	}
+	
+	$pole -> appendChild($contact);
+
+	$dom->formatOutput = true;
+	$dom->normalizeDocument();
+	$dom -> save($file);
+	echo "<br/>Sauvegarde effectuée.<br/>";
+	
+	// Modifier
+	
+	// $noms = $pole -> getElementsByTagName('nom');
+	
+	
+	/*
+	
+	- Ajouter un contact à la fin d'un pôle
+	- Remplacer un contact
+	
+	*/
+
+?>{/source}
